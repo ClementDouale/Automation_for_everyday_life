f36f20e6f5609370d9a564472478c293725c29dd
doualecl <clement.douale@etu.utc.fr>
2024-01-17 13:59:29 +0100

    :construction: new type of files to sort and clean from downloads

--- CleaningFolders/DowloadsFolder/movingFiles.py
+++ CleaningFolders/DowloadsFolder/movingFiles.py
@@ -5,12 +5,20 @@ def move_files_based_on_name(source_folder, destination_folder):
     # Check and create folders in destination
     cover_letters_folder = os.path.join(destination_folder, "Cover Letters")
     cv_folder = os.path.join(destination_folder, "CV")
-
+    courses_folder = os.path.join(destination_folder, "Courses")
+    projects_folder = os.path.join(destination_folder, "Projects")
+    
     if not os.path.exists(cover_letters_folder):
         os.makedirs(cover_letters_folder)
     
     if not os.path.exists(cv_folder):
         os.makedirs(cv_folder)
+        
+    if not os.path.exists(courses_folder):
+        os.makedirs(courses_folder)
+        
+    if not os.path.exists(projects_folder):
+        os.makedirs(projects_folder)
 
     # Scan through files in the source folder
     for file in os.listdir(source_folder):
@@ -21,9 +29,17 @@ def move_files_based_on_name(source_folder, destination_folder):
         if "cover letter" in file_name:
             shutil.move(file_path, cover_letters_folder)
             print(f"Moved '{file}' to 'Cover Letters'")
-        elif "clément douale" in file_name or "clement douale" in file_name:
+        elif "clement douale" in file_name:
             shutil.move(file_path, cv_folder)
             print(f"Moved '{file}' to 'CV'")
+        elif "cours" in file_name:
+            shutil.move(file_path, courses_folder)
+            print(f"Moved '{file}' to 'Courses'")
+        elif "projet" in file_name or "project" in file_name:
+            shutil.move(file_path, projects_folder)
+            print(f"Moved '{file}' to 'Projects'")
+        
+            
 
 # Define source and destination folders
 downloads_folder = os.path.expanduser('~/Downloads')
