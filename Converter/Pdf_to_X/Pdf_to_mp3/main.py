ca974355b43bf7acb886886ae6fd13864b07e372
doualecl <clement.douale@etu.utc.fr>
2024-01-17 12:38:09 +0100

    :construction: creating new ideas

--- Converter/Pdf_to_X/Pdf_to_mp3/main.py
+++ Converter/Pdf_to_X/Pdf_to_mp3/main.py
@@ -1,9 +1,11 @@
 import pyttsx3,PyPDF2
 from PyPDF2 import PdfReader
 
+## Utility of this project : being able to listen to a specific text / book / lesson when we can't focus on reading it
 pdfreader = PdfReader('book.pdf')
 speaker = pyttsx3.init()
 
+## Going through each page of the document until the end
 for page_num in range(len(pdfreader.pages)):
     text = pdfreader.pages[page_num].extract_text()
     clean_text = text.strip().replace('\n', ' ')
